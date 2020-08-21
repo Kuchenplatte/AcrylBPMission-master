f318557c5b644b59cc9c76b747aa866b2bdb5942
Kuchenplatte <40145065+Kuchenplatte@users.noreply.github.com>
2020-08-21 17:05:16 +0200

    Add files via upload

---
+++ @BreakingPointServer/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 1133182382;
+name = "BreakingPointServer";
+timestamp = 5248475900022123390;
---
+++ AcrylGaming.Altis/initplayerlocal.sqf
@@ -0,0 +1,7 @@
+/*
+  Breaking Point Mod for Arma 3
+  Made with Love by ArmaZone.net
+*/
+
+
+
